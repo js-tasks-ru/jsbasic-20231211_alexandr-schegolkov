fffc705f8eb177e7f055a3435eda08601159cce5
Alexander Schegolkov <alexandr.schegolkov@yandex.ru>
2023-12-17 12:17:59 +0300

    2-m-task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,7 @@
 function factorial(n) {
-  // ваш код...
+  let result = 1;
+  for (let i = 1; i < n + 1; i++) {
+    result = result * i;
+  }
+  return result;
 }
--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,10 @@
 function checkSpam(str) {
-  // ваш код...
+  str = str.toLowerCase();
+  if (str.includes("1xbet")) {
+    return true;
+  } else if (str.includes("xxx")) {
+    return true;
+  } else {
+    return false;
+  }
 }
