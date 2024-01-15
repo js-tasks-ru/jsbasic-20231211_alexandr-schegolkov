fbbbc42001a91240749a5a6fb14ae5fbf90c08e1
Alexander Schegolkov <alexandr.schegolkov@yandex.ru>
2024-01-15 15:37:02 +0300

    6 module hometask

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,25 @@
  */
 export default class UserTable {
   constructor(rows) {
+    let table = document.createElement("table");
+    for (let item of rows) {
+      let row = table.insertRow();
+      let cellName = row.insertCell();
+      let cellAge = row.insertCell();
+      let cellSalary = row.insertCell();
+      let cellCity = row.insertCell();
+      let cellCross = row.insertCell();
+      cellName.innerHTML = item["name"];
+      cellAge.innerHTML = item["age"];
+      cellSalary.innerHTML = item["salary"];
+      cellCity.innerHTML = item["city"];
+      cellCross.innerHTML = `<button>[X]</button>`;
+    }
+    table.addEventListener("click", function (event) {
+      if (event.target.closest("td").innerHTML === `<button>[X]</button>`) {
+        event.target.parentNode.parentNode.remove();
+      }
+    });
+    this.elem = table;
   }
 }
--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,35 @@
+import createElement from "../../assets/lib/create-element.js";
+
 export default class ProductCard {
   constructor(product) {
+    this.product = product;
+    this.elem = createElement(`
+    <div class="card">
+    <div class="card__top">
+      <img src="/assets/images/products/${
+        product.image
+      }" class="card__image" alt="product">
+      <span class="card__price">€${product.price.toFixed(2)}</span>
+    </div>
+    <div class="card__body">
+      <div class="card__title">${product.name}</div>
+      <button type="button" class="card__button">
+        <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+      </button>
+    </div>
+  </div>
+`);
+    // let cardButton = document.querySelector(".card__button");
+    let customEvent = new CustomEvent("product-add", {
+      // имя события должно быть именно "product-add"
+      detail: this.product.id, // Уникальный идентификатора товара из объекта товара
+      bubbles: true, // это событие всплывает - это понадобится в дальнейшем
+    });
+
+    this.elem.addEventListener("click", (event) => {
+      if (event.target.closest(".card__button")) {
+        this.elem.dispatchEvent(customEvent);
+      }
+    });
   }
 }
